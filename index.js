bb42c2634ee3190b8a5cc030aec9f8e0c9a3d86c
ecc521 <42424091+ecc521@users.noreply.github.com>
2018-12-04 22:02:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,17 +7,53 @@ window.AudioContext = window.AudioContext || window.webkitAudioContext
 
 
 
+function opentuner() {
+  alert("Tuner under development")
+}
 
-
+function opengenerator() {
+  alert("Note Generator under development")
+}
 
 let displaynavbar = function() {
   let navbar = document.createElement("div")
   navbar.className = "navbar"
   
-  let img = document.createElement("img")
-  img.src = "/tuner/images/settings.png"
-  img.className = "navbaricon"
-  navbar.appendChild(img)
+  let pages = [
+    {
+      draw:opentuner,
+      text:"Tuner",
+      icon:"/tuner/images/settings.png"
+    },
+    {
+      draw:opengenerator,
+      text:"Generator",
+      icon:""
+    }           
+  ]
+  
+  
+  for (let i=0;i<pages.length;i++) {
+    let item = pages[i]
+    
+    let button = document.createElement("btn")
+    button.addEventListener("click", item.draw)
+    
+    let img = document.createElement("img")
+    img.src = item.icon
+    img.className = "navitemicon"
+    
+    let p = document.createElement("p")
+    p.innerHTML = item.text
+    p.className = "navitemtext"
+    
+    button.appendChild(img)
+    button.appendChild(p)
+    
+    navbar.appendChild(button)
+  }
+  
+
   document.body.appendChild(navbar)
 }
 
